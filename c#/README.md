045969379de49bfd7bf7090157ca1ddf88f7acc9
lunar.chen <yue.chen@zentertain.net>
2017-07-17 17:19:19 +0800

    init

---
+++ c#/README.md
@@ -0,0 +1,9 @@
+# hello word
+
+	namespace NamespaceName {
+		class ClassName {
+			static void Main(string[] args) {
+				Console.WriteLine("Hello c#");
+			}
+		}
+	}
---
+++ c#/lab/lab/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace lab
+{
+    class Test
+    {
+
+
+        static void valueTypes(){
+            int intVar = 5; // 值类型 int, float ...
+            Console.WriteLine(intVar);
+
+            object objInt = intVar; // object类型 类似于oc中的id 值变量复制会发生box
+            object sum = Convert.ToInt32(objInt) + 10;
+
+            Console.WriteLine(sum);
+
+            Console.WriteLine("sizeof int " + sizeof(int));
+        }
+
+
+
+        static void Main(string[] args) {
+            valueTypes();
+        }
+    }
+}
+
